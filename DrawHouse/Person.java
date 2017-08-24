4fa175c310d803b0ee709692af62619cd3e86d3e
Vishnu Srinivasan <vishnunaga_srinivasan@yahoo.com>
2017-08-23 17:11:17 -0700

    Added a person to the program

--- DrawHouse/Person.java
+++ DrawHouse/Person.java
@@ -2,18 +2,22 @@ import java.awt.Window;
 
 import processing.core.PApplet;
 public class Person {
- 	public void draw(PApplet drawer) {
+ 	public void draw(PApplet drawerPerson) {
 		
  		
  		// This is the animated figure class and there will be a person or object represented in this class
  		
-		  int height = drawer.height;
-		  int width = drawer.width;
-		  drawer.scale(width/400f,height/300f);
-		  
-		  
-		  
-		  
+		  int height = drawerPerson.height;
+		  int width = drawerPerson.width;
+		  drawerPerson.scale(width/400f,height/300f);
+		  drawerPerson.fill(200, 100, 100);
+
+		  drawerPerson.line(50, 150, 50, 200);
+		  drawerPerson.line(50, 200, 25, 225);
+		  drawerPerson.line(50, 200, 75, 225);
+		  drawerPerson.line(50, 150, 25, 150);
+		  drawerPerson.line(50, 150, 75, 150);
+		  drawerPerson.ellipse(50, 120, 55, 55);;
 		}
 
 }
